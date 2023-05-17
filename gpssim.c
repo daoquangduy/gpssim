84d07d4031322a972733a0e9a2919d427164c94e
Dao Quang Duy <dq-duy@mail.peritec.co.jp>
2023-05-17 14:49:37 +0700

    file bin generate cycle 1s -> 0.5s

--- gpssim.c
+++ gpssim.c
@@ -2515,7 +2515,7 @@ int GPS_create_bin(char* param, char* base_path)
 
 		// add seperate bin file
 
-		if( iumd % 10 == 0){ // 1s loop
+		if( iumd % 5 == 0){ // 1s loop
 
 			// add stop file, read content of stop file, if not NULL --> stop 
 			sprintf(filepath,"%s%s", cwd,"stop.txt");
